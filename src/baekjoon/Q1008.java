ed02cbf4431366725612e4a7c0cc383497b1006f
dani <daeunj255@gmail.com>
2024-08-08 23:44:18 +0900

    initial commit

---
+++ src/baekjoon/Q1008.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        sc.close();
+
+        System.out.println(A * 1.0 / B);
+    }
+}
