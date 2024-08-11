1217bfbfa8ad69509b688f8d4e96490455e08d0d
dani <daeunj255@gmail.com>
2024-08-11 21:16:58 +0900

    baekjoon solved.ac class1

---
+++ src/baekjoon/Q10871.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q10871 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int X = scanner.nextInt();
+        int[] array = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        scanner.close();
+
+        for (int n : array) {
+            if (n < X) {
+                System.out.print(n + " ");
+            }
+        }
+    }
+}
