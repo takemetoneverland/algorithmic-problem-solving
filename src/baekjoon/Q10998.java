57b03e0412ceff119d215ba3e9ace2552f7c4bfb
dani <daeunj255@gmail.com>
2024-08-13 22:13:27 +0900

    baekjoon solved.ac class1

---
+++ src/baekjoon/Q10998.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q10998 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        scanner.close();
+
+        System.out.println(A * B);
+    }
+}
