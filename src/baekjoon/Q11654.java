acf01259497574c30274df3dd2a24925cfbbe138
dani <daeunj255@gmail.com>
2024-08-14 23:42:53 +0900

    baekjoon solved.ac class1

---
+++ src/baekjoon/Q11654.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q11654 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.nextLine();
+        char ch = str.charAt(0);
+
+        System.out.println(ch + 0);
+    }
+}
