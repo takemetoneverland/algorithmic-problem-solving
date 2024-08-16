90f04ca6b069fc14e4a2f379145e299d03188ddf
dani <daeunj255@gmail.com>
2024-08-16 19:55:03 +0900

    baekjoon solved.ac class1

---
+++ src/baekjoon/Q27866.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q27866 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String S = scanner.nextLine();
+        int i = scanner.nextInt();
+
+        System.out.println(S.charAt(i-1));
+    }
+}
