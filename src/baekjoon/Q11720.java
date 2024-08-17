5db6b0d8b6e70ce1b18b21bd6c4e5fd2981b6c5e
dani <daeunj255@gmail.com>
2024-08-17 21:53:29 +0900

    baekjoon solved.ac class1

---
+++ src/baekjoon/Q11720.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q11720 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        scanner.nextLine();
+        String number = scanner.nextLine();
+        char[] charArr = number.toCharArray();
+
+        int total = 0;
+        for (char ch : charArr) {
+            total += Character.getNumericValue(ch);
+        }
+
+        System.out.println(total);
+    }
+}
