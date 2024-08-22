8df13a3f6ab117d14aa09ade15fc94e9d669dc10
takemetoneverland <daeunj255@gmail.com>
2024-08-22 12:47:22 +0900

    [Bronze II] Title: 음계, Time: 168 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[8];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int[] ascending = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] descending = {8, 7, 6, 5, 4, 3, 2, 1};
+
+        if (Arrays.equals(array, ascending)) {
+            System.out.println("ascending");
+        } else if (Arrays.equals(array, descending)) {
+            System.out.println("descending");
+        } else {
+            System.out.println("mixed");
+        }
+    }
+}
