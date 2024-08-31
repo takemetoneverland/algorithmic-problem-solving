4db6717fdbcafb1ba60535bea7a51ac1a8ce5404
takemetoneverland <daeunj255@gmail.com>
2024-09-01 02:10:27 +0900

    [Bronze IV] Title: R2, Time: 176 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int R1 = sc.nextInt();
+        int S = sc.nextInt();
+        //R2 = S * 2 - R1;
+
+        System.out.println(S * 2 - R1);
+    }
+}
