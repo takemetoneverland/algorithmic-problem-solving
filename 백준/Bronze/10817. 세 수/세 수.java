e34ecbae27077ebd7172cda0f2f692ab78990338
takemetoneverland <daeunj255@gmail.com>
2024-09-01 09:49:52 +0900

    [Bronze III] Title: 세 수, Time: 168 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        Arrays.sort(numbers);
+        System.out.println(numbers[1]);
+    }
+}
