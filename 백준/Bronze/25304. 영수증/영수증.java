f273ba46ab696bfdd1ebb20ea27ef2725cd22cb8
takemetoneverland <daeunj255@gmail.com>
2024-09-01 09:14:22 +0900

    [Bronze IV] Title: 영수증, Time: 96 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int X = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int totalPrice = 0;
+
+        while (N-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            totalPrice += a * b;
+        }
+
+        System.out.println(X == totalPrice ? "Yes" : "No");
+    }
+}
